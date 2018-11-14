0b335268c46baacc6ed3736cfec98a3b78e45fc1
Julia Jobson <juljo192@student.liu.se>
2018-11-14 10:01:04 +0100

    Stuck at mapping positions to forces

--- import_data.py
+++ import_data.py
@@ -6,14 +6,24 @@ import sys
 import time
 
 import numpy as np
+from qml.representation import get_slatm_mbtypes
 
 import qml
 
 
 
-def createCompund(num_files)
+def createCompound(num_files)
     """Creates coumpund objects from each xyz-file"""
 
 
-    compounds = [qml.Compound(xyz="qm/%s.xyz" % str(d).zfill(5)) for d in range(num_files)]
+    compound_list = [qml.Compound(xyz="qm/%s.xyz" % str(d).zfill(5)) for d in range(num_files)]
+    return compound_list
+
+
 if __name__ == '__main__':
+    compounds = createCompound(5)
+    mbtypes = get_slatm_mbtypes([atoms.nuclear_charges for atoms in compounds])
+
+    for atoms in compounds:
+        atoms.generate_slatm(atoms, mbtypes)
+    
