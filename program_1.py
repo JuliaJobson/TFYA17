05c5cf7657d15c38704fc7d148412ffb3c6ab12d
Frida Svensson <frisv135@hunn-02.edu.mai.liu.se>
2018-11-27 12:01:14 +0100

    retoch program 1

--- program_1.py
+++ program_1.py
@@ -9,6 +9,7 @@ import numpy as np
 from qml.representations import get_slatm_mbtypes
 from qml.kernels import get_local_kernels_gaussian
 from qml.math import cho_solve
+from math import floor
 
 import qml
 
@@ -31,33 +32,34 @@ def generateRep(num_of_xyz, property_filename):
 
 if __name__ == "__main__":
 
+    num_xyz = 10000
+
     # For every compound generate a coulomb matrix
-    compounds = generateRep(400, 'infile.forces')
+    compounds = generateRep(num_xyz, 'infile.forces')
 
     # Make a big 2D array with all the
     X = np.concatenate([atoms.representation for atoms in compounds])
     N = np.array([atoms.natoms for atoms in compounds])
     Y = np.array([atoms.properties for atoms in compounds])
 
-    print(X.shape)
-    print(Y.shape)
+
+    # variables
+    num_atoms = N[0]
+    num_comp = floor(num_xyz/2)
+
 
     # Assign 1000 first atoms to the training set
-    X_training = X[:200*N[0], :]
-    Y_training = Y[:200, :, :]
+    X_training = X[:num_comp*num_atoms, :]
+    Y_training = Y[:num_comp, :, :]
     print('X_training')
     print(X_training.shape)
     #print(Y_training)
 
 
-    # Assign 1000 last atoms to the training set
-    #X_test = X[-1000:]
-    #Y_test = Y[-1000:]
-
 
     # Calculate the Gaussian kernel
     sigmas =  [1000.0]
-    K = get_local_kernels_gaussian(X_training, X_training, N[:200], N[:200], sigmas)
+    K = get_local_kernels_gaussian(X_training, X_training, N[:num_comp], N[:num_comp], sigmas)
     print('K')
     print(K.shape)
 
@@ -66,29 +68,30 @@ if __name__ == "__main__":
 
 
     # Use the built-in Cholesky-decomposition to solve
-    alphas=np.zeros([200, N[0], 3])
-    for i in range(N[0]):
+    alphas=np.zeros([num_comp, num_atoms, 3])
+    for i in range(num_atoms):
         for j in range(3):
             alphas[:,i,j] = cho_solve(K[0,:,:], Y_training[:,i,j])
     print('alphas')
     print(alphas.shape)
 
-    # Assign 1000 last molecules to the test set
-    X_test = X[-200*32:, :]
-    Y_test = Y[-200:, : , :]
+    # Assign last atoms to the test set
+    X_test = X[-num_comp*num_atoms:, :]
+    Y_test = Y[-num_comp:, : , :]
     print('X-test')
     print(X_test.shape)
 
     # calculate a kernel matrix between test and training data, using the same sigma
-    Ks = get_local_kernels_gaussian(X_training, X_test, N[:200], N[-200:], sigmas)
+    Ks = get_local_kernels_gaussian(X_training, X_test, N[:num_comp], N[-num_comp:], sigmas)
     print('Ks')
     print(Ks.shape)
 
     # Make the predictions
-    Y_predicted = np.zeros([200, 32, 3])
-    for i in range(N[0]):
+    Y_predicted = np.zeros([num_comp, num_atoms, 3])
+    for i in range(num_atoms):
         for j in range(3):
             Y_predicted[:,i,j] = np.dot(Ks[0,:,:], alphas[:,i,j])
+    print('Y_predicted')
     print(Y_predicted.shape)
     # Calculate mean-absolute-error (MAE):
     print(np.mean(np.abs(Y_predicted - Y_test)))
